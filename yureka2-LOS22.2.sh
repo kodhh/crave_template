a1a45c736e6c645db0a5d9abe0adfa6f14a46fd2
kodhh <89465249+kodhh@users.noreply.github.com>
2026-06-21 15:16:55 +0800

    Update yureka2-LOS22.2.sh

--- yureka2-LOS22.2.sh
+++ yureka2-LOS22.2.sh
@@ -1,19 +1,12 @@
-rm -rf .repo/local_manifests 
-
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --depth=1 --git-lfs
 
-/opt/crave/resync.sh 
+rm -rf .repo/local_manifests/* kernel/yu/YUREKA2 device/yu/YUREKA2 vendor/yu/YUREKA2 hardware/qcom-caf/msm8953/audio hardware/qcom-caf/msm8953/display hardware/qcom-caf/msm8953/media 2>/dev/null
 
-rm -rf kernel/yu/YUREKA2 device/yu/YUREKA2 vendor/yu/YUREKA2 2>/dev/null
-rm -rf hardware/qcom-caf/msm8953/audio hardware/qcom-caf/msm8953/display hardware/qcom-caf/msm8953/media 2>/dev/null
+mkdir -p .repo/local_manifests
 
-git clone https://github.com/kodhh/android_kernel_YUREKA2-4.19.git -b lineageos-22.2 --depth=1 kernel/yu/YUREKA2
-git clone https://github.com/kodhh/device-yureka2.git -b lineageos-22.2 --depth=1 device/yu/YUREKA2
-git clone https://github.com/kodhh/vendor-yureka2.git -b lineageos-22.2 --depth=1 vendor/yu/YUREKA2
-git clone https://github.com/kodhh/android_hardware_qcom_audio_mithorium.git -b master --depth=1 hardware/qcom-caf/msm8953/audio
-git clone https://github.com/kodhh/android_hardware_qcom_display_mithorium.git -b master --depth=1 hardware/qcom-caf/msm8953/display
-git clone https://github.com/kodhh/android_hardware_qcom_media_mithorium.git --depth=1 hardware/qcom-caf/msm8953/media
+curl -sL https://raw.githubusercontent.com/kodhh/crave_template/refs/heads/master/yureka2_roomservice.xml -o .repo/local_manifests/roomservice.xml
 
+/opt/crave/resync.sh
 
 source build/envsetup.sh
 breakfast YUREKA2 userdebug
