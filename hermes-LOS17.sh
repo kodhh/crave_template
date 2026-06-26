b2eb10c1aa4d170ef88605e2bf09338f1a5ff10c
kodhh <89465249+kodhh@users.noreply.github.com>
2026-06-26 08:44:27 +0800

    Update hermes-LOS17.sh

--- hermes-LOS17.sh
+++ hermes-LOS17.sh
@@ -1,29 +1,47 @@
+if [ -d "device/xiaomi/hermes" ]; then
+		pwdir=$(pwd)
+		rm -rf external/chromium-webview/prebuilt/arm external/chromium-webview/prebuilt/arm64 external/chromium-webview/prebuilt/x86 external/chromium-webview/prebuilt/x86_64
+		rm -rf out/target/product/hermes/obj/APPS/webview_intermediates
+		git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_x86_64 external/chromium-webview/prebuilt/x86_64 --depth=1
+		git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_x86 external/chromium-webview/prebuilt/x86 --depth=1
+		git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_arm64 external/chromium-webview/prebuilt/arm64 --depth=1
+		git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_arm external/chromium-webview/prebuilt/arm --depth=1
+		rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+		rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
+		git clone --depth=1 -b lineage-17.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+		git clone --depth=1 -b lineage-17.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
+        cd device/xiaomi/hermes
+        git pull https://github.com/kodhh/android_device_tree_hermes.git lineage-17.1
+        git reset f3b84a459b88a32d3daf690a3e70380f0956a57b --hard
+		cd "$pwdir"
+		timeout 120 bash -c "device/xiaomi/hermes/patches2/install.sh"
+else
+		repo init -u https://github.com/LineageOS/android.git -b lineage-17.1 --git-lfs --no-repo-verify
+		rm -rf external/libmojo external/libchrome external/nos prebuilts/clang/host/darwin-x86 prebuilts/clang/host/linux-x86 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 prebuilts/gcc/linux-x86/host/x86_64-w64-mingw32-4.8 prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.9 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
+		rm -rf kernel/xiaomi/hermes device/xiaomi/hermes vendor/xiaomi/hermes 2>/dev/null
+		rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+		rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-17.1 --git-lfs --depth=1 --no-repo-verify
-/opt/crave/resync.sh 
+		/opt/crave/resync.sh
 
-rm -rf kernel/xiaomi/hermes device/xiaomi/hermes vendor/xiaomi/hermes 2>/dev/null
-rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-rm -rf prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
-rm -rf external/chromium-webview/prebuilt/arm external/chromium-webview/prebuilt/arm64 external/chromium-webview/prebuilt/x86 external/chromium-webview/prebuilt/x86_64 -rf
+		rm -rf external/chromium-webview/prebuilt/arm external/chromium-webview/prebuilt/arm64 external/chromium-webview/prebuilt/x86 external/chromium-webview/prebuilt/x86_64
+		rm -rf out/target/product/hermes/obj/APPS/webview_intermediates
+		git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_x86_64 external/chromium-webview/prebuilt/x86_64 --depth=1
+		git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_x86 external/chromium-webview/prebuilt/x86 --depth=1
+		git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_arm64 external/chromium-webview/prebuilt/arm64 --depth=1
+		git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_arm external/chromium-webview/prebuilt/arm --depth=1
 
-git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_x86_64 external/chromium-webview/prebuilt/x86_64 --depth=1
-git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_x86 external/chromium-webview/prebuilt/x86 --depth=1
-git clone external/chromium-webview/prebuilt/arm64 external/chromium-webview/prebuilt/arm64 --depth=1
-git clone external/chromium-webview/prebuilt/arm  external/chromium-webview/prebuilt/arm --depth=1
+		git clone https://github.com/kodhh/android_kernel_xiaomi_hermes.git -b lineage-16.0 --depth=1 kernel/xiaomi/hermes
+		git clone https://github.com/kodhh/android_device_tree_hermes.git -b lineage-17.1 --depth=1 device/xiaomi/hermes
+		git clone https://github.com/kodhh/android_vendor_hermes.git -b lineage-17.1 --depth=1 vendor/xiaomi/hermes
+		git clone --depth=1 -b lineage-17.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+		git clone --depth=1 -b lineage-17.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
 
-git clone https://github.com/kodhh/android_kernel_xiaomi_hermes.git -b lineage-16.0 --depth=1 kernel/xiaomi/hermes
-git clone https://github.com/kodhh/android_device_tree_hermes.git -b lineage-17.1 --depth=1 device/xiaomi/hermes
-git clone https://github.com/kodhh/android_vendor_hermes.git -b lineage-17.1 --depth=1 vendor/xiaomi/hermes
+		timeout 120 bash -c "device/xiaomi/hermes/patches/install.sh"
+		timeout 120 bash -c "device/xiaomi/hermes/patches2/install.sh"
 
-git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
-
-timeout 120 bash -c "device/xiaomi/hermes/patches/install.sh"
+fi
 
 source build/envsetup.sh
 breakfast hermes
-
-make -j$(nproc) SIGN_IMAGE=1 SHOW_COMMANDS=1 -- -k 0
-
-mka bacon
+make -j$(nproc) SHOW_COMMANDS=1 -k bacon
